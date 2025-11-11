b16ce9a59fd2aad27ae0cf77976462d1b2c794b6
osuiso-depot <85674477+osuiso-depot@users.noreply.github.com>
2025-11-12 01:28:02 +0900

    fixed xy plot error

--- modules/images.py
+++ modules/images.py
@@ -168,7 +168,7 @@ def draw_grid_annotations(im, width, height, hor_texts, ver_texts, margin=0):
         for line in lines:
             fnt = initial_fnt
             fontsize = initial_fontsize
-            while drawing.multiline_textsize(line.text, font=fnt)[0] > line.allowed_width and fontsize > 0:
+            while drawing.multiline_textbbox((0, 0), line.text, font=fnt)[2] > line.allowed_width and fontsize > 0:
                 fontsize -= 1
                 fnt = get_font(fontsize)
             drawing.multiline_text((draw_x, draw_y + line.size[1] / 2), line.text, font=fnt, fill=color_active if line.is_active else color_inactive, anchor="mm", align="center")
