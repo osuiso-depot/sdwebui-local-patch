3b759b4534a8aca6b74293a5ce14d61bd786d0c8
osuiso-depot <85674477+osuiso-depot@users.noreply.github.com>
2025-11-12 01:29:06 +0900

    update bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--skip-install --no-download-sd-model --xformers --force-enable-xformers --configpresets-dir "C:\Users\2021NSAPU\Documents\GitHub\MySDWEBUI_config_private"
+REM set COMMANDLINE_ARGS=--no-download-sd-model --xformers --force-enable-xformers --configpresets-dir "C:\Users\2021NSAPU\Documents\GitHub\MySDWEBUI_config_private"
 
 call webui.bat
